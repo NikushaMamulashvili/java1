dfb2a1298b3a84bff319d9a32fc5276885a9b277
Nika Mamulashvili <164235570+NikushaMamulashvili@users.noreply.github.com>
2024-05-25 14:48:31 +0400

    Add files via upload

---
+++ Java1/script.js
@@ -0,0 +1,10 @@
+
+let age = 16
+let hobby = "touch_grass"
+let name = "Nika"
+let birthday =2008
+
+console.log("my hobby is" +hobby)
+console.log("my name is" +name)
+console.log("my year of birth is " +birthday)
+console.log("my current age is " +age)
